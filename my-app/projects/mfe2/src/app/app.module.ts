c27a8b8428ad6b387fadc9b9d47f4ed23a08adb1
Yathartha <varshneya.yathartha@gmail.com>
2023-09-20 21:34:21 +0530

    mfe2 static MF working

--- my-app/projects/mfe2/src/app/app.module.ts
+++ my-app/projects/mfe2/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightsModule2 } from './flights/flights.module';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FlightsModule2,
     AppRoutingModule
   ],
   providers: [],
