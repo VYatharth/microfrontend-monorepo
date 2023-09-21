ed8b129a09194041750a5abbe731e717c7e990ba
Yathartha <varshneya.yathartha@gmail.com>
2023-09-21 12:30:19 +0530

    Updated webpack config and bootstrapping of shell app

--- my-app/projects/shell/webpack.config.js
+++ my-app/projects/shell/webpack.config.js
@@ -1,12 +1,11 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
-  remotes: {
-    "mfe1": "http://localhost:4201/remoteEntry.js",
-    "mfe2": "http://localhost:4202/remoteEntry.js",    
-  },
-
+  // not needed for dynamic MF
+  // remotes: {
+  //   "mfe1": "http://localhost:4201/remoteEntry.js",
+  //   "mfe2": "http://localhost:4202/remoteEntry.js",    
+  // },
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
