40d449c2fcfd6703a6eb2915c7b818f4019821a5
Yathartha <varshneya.yathartha@gmail.com>
2023-09-21 12:31:13 +0530

    Updated routing for dynamic Module federation

--- my-app/projects/shell/src/app/app-routing.module.ts
+++ my-app/projects/shell/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 const routes: Routes = [ {
     path: '',
@@ -10,11 +11,21 @@ const routes: Routes = [ {
   },
   {
     path: 'flights',
-    loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
+    loadChildren: () => loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'mfe1',
+        exposedModule: './Module'
+      })
+      .then(m => m.FlightsModule)
   },
   {
     path: 'mfe2',
-    loadChildren: () => import('mfe2/Module').then(m => m.FlightsModule2)
+    loadChildren: () => loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'mfe2',
+        exposedModule: './Module'
+      })
+      .then(m => m.FlightsModule2)
   },
   {
     path: '**',
